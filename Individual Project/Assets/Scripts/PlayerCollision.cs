0e710924afa8165950f0a014bd52c59b2b3d54f7
hanadigipen <106277037+hanadigipen@users.noreply.github.com>
2023-05-12 10:08:32 -0700

    backup

---
+++ Individual Project/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+
+}
